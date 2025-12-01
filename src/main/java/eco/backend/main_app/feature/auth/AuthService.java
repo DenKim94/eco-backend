706363dfbac7d07bb093b3fc838869318a980de1
Denis Kim <denis-kim@outlook.de>
2025-12-01 18:09:14 +0100

    Updated AuthService

--- src/main/java/eco/backend/main_app/feature/auth/AuthService.java
+++ src/main/java/eco/backend/main_app/feature/auth/AuthService.java
@@ -1,6 +1,8 @@
 package eco.backend.main_app.feature.auth;
 
+import eco.backend.main_app.core.exception.GenericException;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import eco.backend.main_app.feature.auth.model.UserEntity;
@@ -19,9 +21,12 @@ public class AuthService {
 
     @Transactional
     public void register(String username, String rawPassword) {
-        // 1. Validierung: Gibt es den User schon?
+        // 1. Prüfung: Gibt es den User schon?
         if (userRepository.existsByUsername(username)) {
-            throw new IllegalArgumentException("Username already taken: " + username);
+            throw new GenericException(
+                "Provided username '" + username + "' already exists.",
+                HttpStatus.CONFLICT // Status 409
+            );
         }
 
         // 2. HASHING
